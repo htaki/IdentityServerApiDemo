db823891bc59c4fbb9ccc1772999738b250a8e85
Hicham <Taki.Hicham.01@outlook.com>
2020-05-05 02:31:44 +0200

    Creating the API project

--- IdentityServerApiDemo.API/Startup.cs
+++ IdentityServerApiDemo.API/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IdentityServerApiDemo.API.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -9,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
 
 namespace IdentityServerApiDemo.API
 {
@@ -24,7 +26,9 @@ namespace IdentityServerApiDemo.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<BankContext>(options => options.UseInMemoryDatabase("bankDB"));
             services.AddControllers();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
